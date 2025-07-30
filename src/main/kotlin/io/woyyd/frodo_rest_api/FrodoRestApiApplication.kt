2b892dda5a28a7b02f69cf2604afb0044598736b
Woyyd <woyyd@protonmail.com>
2025-07-30 12:27:46 +0200

    forgot to add

---
+++ src/main/kotlin/io/woyyd/frodo_rest_api/FrodoRestApiApplication.kt
@@ -0,0 +1,11 @@
+package io.woyyd.frodo_rest_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrodoRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FrodoRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/io/woyyd/frodo_rest_api/FrodoRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.woyyd.frodo_rest_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FrodoRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
